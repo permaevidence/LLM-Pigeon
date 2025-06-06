1c604bf9676ada2abdb662d42ce37642dbf2f1d5
matteo ianni <iannimatte@gmail.com>
2025-06-06 08:41:17 -0400

    Initial Commit

---
+++ Pigeon Chat/Pigeon_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pigeon_ChatApp.swift
+//  Pigeon Chat
+//
+//  Created by matteo ianni on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Pigeon_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
